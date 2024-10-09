b6565e09a01a9658e77b4a68b14a22c385378797
Khwezbuisnes <168282778+Khwezbuisnes@users.noreply.github.com>
2024-10-09 02:49:38 +0200

    Update ordered.sh

--- ordered.sh
+++ ordered.sh
@@ -1,19 +1,29 @@
+#!/bin/bash
 
+#variable declaration
 start=$1
 end=$2
 from=$3
 
 count=1
 
+#determin file quantity
 file_count=$(ls -1 $start | wc -l)
 
+#main loop
 for count in $(seq ${count} $file_count); do
 
+#determin file name
 file=$(ls -1 $start | sed -n "${count}p")
 
-echo $file copied from $start to $end as $from
+#determin file type
+ftype=${file##*.}
 
-cp $start/$file $end/${from}.png
+#declareing actions
+echo cpoying $file from $start to $end as $from
+
+#transfer
+cp $start/$file $end/${from}.$ftype
 ((from++))
 ((count++))
 
